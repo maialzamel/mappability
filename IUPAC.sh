46128cbda90acd61d7c3e469a73d57486141b7cc
Mai Alzamel <k1505471@kcl.ac.uk>
2018-11-20 17:47:55 +0000

    Add files via upload

---
+++ IUPAC.sh
@@ -0,0 +1,23 @@
+
+#! /usr/bin/bash
+
+
+sed -i 's/Y/C/g' ./data/GCF_000167295.fasta
+
+sed -i 's/K/G/g' ./data/GCF_000167295.fasta
+
+sed -i 's/M/A/g' ./data/GCF_000167295.fasta
+
+sed -i 's/S/C/g' ./data/GCF_000167295.fasta
+
+sed -i 's/W/A/g' ./data/GCF_000167295.fasta
+
+sed -i 's/V/T/g' ./data/GCF_000167295.fasta
+
+sed -i 's/N/A/g' ./data/GCF_000167295.fasta
+
+
+sed -i 's/R/A/g' ./data/GCF_000167295.fasta
+
+
+
